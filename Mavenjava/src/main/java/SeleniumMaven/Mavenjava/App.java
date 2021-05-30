017f012561c12f53d763b33825d8bf59937c5573
karthik <karthiksdkd@gmail.com>
2021-05-30 19:19:50 +0530

    second commit

---
+++ Mavenjava/src/main/java/SeleniumMaven/Mavenjava/App.java
@@ -0,0 +1,13 @@
+package SeleniumMaven.Mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
